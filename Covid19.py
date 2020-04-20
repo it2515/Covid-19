b700228d234d627c5f71d325a03523dc43d9d88b
Christian Thummel <49539167+it2515@users.noreply.github.com>
2020-04-20 13:38:25 -0500

    Add files via upload

--- Covid19.py
+++ Covid19.py
@@ -5,24 +5,33 @@ import pandas as pd
 import requests
 import random
 from itertools import count
+# useful data
 
+
+# Get data on only confirmed cases
+#api_response = requests.get('https://covid19api.herokuapp.com/confirmed')
+ 
+# Print latest data for location ID 100: California, USA
+#print(api_response.json()['latest'])
+
+
+#import requests
 
 # Request fails unless we provide a user-agent
 api_response = requests.get('https://api.thevirustracker.com/free-api?countryTimeline=US', headers={"User-Agent": "Chrome"})
 covid_stats = api_response.json()['timelineitems']
  
 # Break out individual stats
-
-
-daily_deaths =[]
-total_casesL = []
-daily_cases = []
 date= []
-
 for i in covid_stats:
     print (i)
     del i['stat']
    
+daily_deaths =[]
+total_casesL = []
+daily_cases = []
+y = []
+z = 0
 for c_date, info in i.items():
     print("\nDate:", c_date)
     date.append(c_date)
@@ -30,16 +39,28 @@ for c_date, info in i.items():
     total_casesL.append(info['total_cases'])
     print ('New Cases:',info['new_daily_cases'])
     daily_cases.append(info['new_daily_cases'])
-    print ('New Deaths:',info['new_daily_deaths'])
     daily_deaths.append(info['new_daily_deaths'])
+
 print(total_casesL) 
 print(daily_cases)   
 print(daily_deaths) 
 print (date)
 
-plt.style.use('fivethirtyeight')
-plt.plot(date, total_casesL )
-plt.tight_layout()
+x = len(date)
+
+for l in date :
+    l = z 
+    z = l+1
+    y.append(z)
+plt.plot(y,total_casesL)    
+plt.plot(y,daily_cases)
+plt.plot(y, daily_deaths)
+plt.ylabel('People')
+plt.xlabel('Days') 
+plt.title('Daliy Covid 19 Cases')
+
+
+plt.legend(['New Cases', ' New Deaths'])
 plt.show()
 
 
