1510fb480d0e332a6dc9ebeff620622ec0ffbcd6
Christian Thummel <49539167+it2515@users.noreply.github.com>
2020-04-20 19:43:21 -0500

    Update Covid19.py

--- Covid19.py
+++ Covid19.py
@@ -5,17 +5,6 @@ import pandas as pd
 import requests
 import random
 from itertools import count
-# useful data
-
-
-# Get data on only confirmed cases
-#api_response = requests.get('https://covid19api.herokuapp.com/confirmed')
- 
-# Print latest data for location ID 100: California, USA
-#print(api_response.json()['latest'])
-
-
-#import requests
 
 # Request fails unless we provide a user-agent
 api_response = requests.get('https://api.thevirustracker.com/free-api?countryTimeline=US', headers={"User-Agent": "Chrome"})
@@ -23,15 +12,18 @@ covid_stats = api_response.json()['timelineitems']
  
 # Break out individual stats
 date= []
-for i in covid_stats:
-    print (i)
-    del i['stat']
 deaths = []  
 daily_deaths =[]
 total_casesL = []
 daily_cases = []
 y = []
 z = 0
+
+
+for i in covid_stats:
+    print (i)
+    del i['stat']
+
 for c_date, info in i.items():
     print("\nDate:", c_date)
     date.append(c_date)
@@ -41,30 +33,30 @@ for c_date, info in i.items():
     daily_cases.append(info['new_daily_cases'])
     daily_deaths.append(info['new_daily_deaths'])
     deaths.append(info['total_deaths'])
+
 print(total_casesL) 
 print(daily_cases)   
 print(daily_deaths) 
 print (date)
+print (plt.style.available)
 
-x = len(date)
+death_rate = deaths[-1]/total_casesL[-1]
+c = str(death_rate)
+print('Death rate: ' + c[1:6])
 
 for l in date :
     l = z 
     z = l+1
     y.append(z)
-
-print (plt.style.available)    
+ 
 plt.plot(y,total_casesL, label = 'Cases', marker = '.', linewidth=3  )    
 plt.plot(y,daily_cases , 'y', label = 'New Cases', linestyle = '--', )
 plt.plot(y, daily_deaths, 'k', label = 'New Deaths' )
 plt.plot(y, deaths, color = 'r', label = 'Deaths' ,  )  
+
 plt.ylabel('People')
 plt.xlabel('Days') 
 plt.title('Daliy Covid 19 Cases (USA)')
-death_rate = deaths[-1]/total_casesL[-1]
-c = str(death_rate)
-
-print('Death rate: ' + c[2:6])
 plt.tight_layout()
 plt.grid(True)
 plt.legend()
